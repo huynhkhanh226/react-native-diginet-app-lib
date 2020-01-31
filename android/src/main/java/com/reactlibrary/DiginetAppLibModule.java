dd510a5d65934ce9d2d7f1267a84805f09f7c946
HUYNH KHANH <huynhkhanh@diginet.com.vn>
2020-01-31 10:59:43 +0700

    Khanh commit new

--- android/src/main/java/com/reactlibrary/DiginetAppLibModule.java
+++ android/src/main/java/com/reactlibrary/DiginetAppLibModule.java
@@ -4,6 +4,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import android.widget.Toast;
 
 public class DiginetAppLibModule extends ReactContextBaseJavaModule {
 
@@ -26,7 +27,7 @@ public class DiginetAppLibModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public String hello(String stringArgument) {
-        return "Hello " + stringArgument;
-    }
+  public void show(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
 }
